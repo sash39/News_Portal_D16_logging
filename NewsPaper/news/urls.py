0e41e48de2629a300a849650d1974f238d01dbf5
sash39 <aleksandr.egorkin@gmail.com>
2022-11-27 13:01:36 +0200

    Cache

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -7,7 +7,7 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('', PostsList.as_view(), name='news_list'),
-    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('<int:pk>', cache_page(60)(PostDetail.as_view()), name='post_detail'),
     path('create/', PostCreate.as_view(), name='post_create'),
     path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
     path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
